a9eb258f4cdc4209d7c0d831b9d2e8760a602e7c
Mohammed Elgaily <mbajily@gmail.com>
2024-11-07 15:44:23 +0000

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,7 +3,7 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields, redaction, message, separator):
     """Obfuscates the specified fields in a log message."""
     for field in fields:
         a = fr"{field}=[^{separator}]*"
